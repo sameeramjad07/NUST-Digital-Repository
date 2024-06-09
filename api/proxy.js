9f0ffb2f5a9a4287834f525290731ab36ea91abd
Muhammad Sameer Amjad <proton7ve@gmail.com>
2024-06-09 17:11:43 +0500

    configured proxy issues

--- api/proxy.js
+++ api/proxy.js
@@ -4,20 +4,21 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 export default async function handler(req, res) {
-    const baseUrl = process.env.VITE_QALAM_BASE_URL; // Ensure this is set in Vercel envs
-    const apiUrl = `${baseUrl}${req.url.replace('/api/proxy', '')}`;
+  const { path } = req.query;
+  const baseUrl = process.env.VITE_QALAM_BASE_URL;
+  const apiUrl = `${baseUrl}/${path.join('/')}`;
 
-    try {
-        const response = await axios({
-          url: apiUrl,
-          method: req.method,
-          headers: {
-            ...req.headers,
-            host: undefined, // Remove the host header
-          },
-          params: req.query,
-          data: req.body,
-        });
+  try {
+    const response = await axios({
+      url: apiUrl,
+      method: req.method,
+      headers: {
+        ...req.headers,
+        host: undefined,
+      },
+      params: req.query,
+      data: req.body,
+    });
 
     res.status(response.status).json(response.data);
   } catch (error) {
